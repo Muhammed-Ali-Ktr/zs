30d6cd8046b8f163335e48aeffa2f683aa62abd3
Muhammed-Ali-Ktr <muhammedalikitir.tr@gmail.com>
2025-08-27 01:09:03 +0300

    Update technologies.js

--- technologies.js
+++ technologies.js
@@ -1,1 +1,10 @@
-a
+function toggleInfo(btn) {
+  const infoBox = btn.nextElementSibling;
+  if (infoBox.style.display === "block") {
+    infoBox.style.display = "none";
+    btn.textContent = "Daha Fazla Bilgi";
+  } else {
+    infoBox.style.display = "block";
+    btn.textContent = "Daha Az Bilgi";
+  }
+}
